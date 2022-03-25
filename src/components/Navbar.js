e6aa2475c4e01aa86db69557ac48b08d178033fc
rohitmaheshwari <49793237+rohitmaheshwari@users.noreply.github.com>
2022-03-25 11:06:59 +0500

    v1

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = ({ filterItem, menuList }) => {
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='btn-group'>
+                    {menuList.map((category) => {
+                        return (
+                            <button className='btn-group__item' onClick={() => filterItem(category)}>{category}</button>
+                        );
+                    })}
+                    {/* <button className='btn-group__item' onClick={() => filterItem("breakfast")}>Breakfast</button>
+                    <button className='btn-group__item' onClick={() => filterItem("lunch")}>Lunch</button>
+                    <button className='btn-group__item' onClick={() => filterItem("evening")}>Evening</button>
+                    <button className='btn-group__item' onClick={() => filterItem("dinner")}>Dinner</button> */}
+                    {/* <button className='btn-group__item' onClick={() => setMenuData(Menu)}>All</button> */}
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
